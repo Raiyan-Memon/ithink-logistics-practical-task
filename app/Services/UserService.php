9988fbcd213ad02914d4582e4674a46e1e91e59f
Raiyan Memon <raiyanmemon7860@gmail.com>
2025-02-15 00:17:00 +0530

    Cache Impletemted

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,31 +2,46 @@
 namespace App\Services;
 
 use App\Models\User;
+use Cache;
 use Illuminate\Support\Facades\Hash;
 
 class UserService
 {
+    public $userCacheName = "user_";
+
     public function create($userData)
     {
-        User::create([
-            'name'     => $userData['name'],
-            'email'    => $userData['email'],
-            'password' => Hash::make($userData['password']),
-        ]);
+        $storeUser           = new User;
+        $storeUser->name     = $userData['name'];
+        $storeUser->email    = $userData['email'];
+        $storeUser->password = Hash::make($userData['password']);
+        $storeUser->save();
     }
 
     public function get($userId)
     {
-        return User::findOrFail($userId);
+        $cache = $this->userCacheName;
+        return Cache::rememberForever("$cache$userId", function () use ($userId) {
+            return User::find($userId);
+        });
     }
 
     public function update($userData)
     {
-        $this->get($userData['id'])->update(['name' => $userData['name'], 'email' => $userData['email']]);
+        $userId = $userData['id'];
+        $this->get($userId)->update(['name' => $userData['name'], 'email' => $userData['email']]);
+        $this->clearUserCache($userId);
     }
 
     public function delete($id)
     {
         $this->get($id)->delete();
+        $this->clearUserCache($id);
+    }
+
+    private function clearUserCache($userId)
+    {
+        $cache = $this->userCacheName;
+        Cache::forget("$cache$userId");
     }
 }
